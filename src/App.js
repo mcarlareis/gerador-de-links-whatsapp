de0d99550b61bb2480ccc38c17bd47a50766dbe1
Maria Carla <maria.carla@poli.digital>
2022-04-13 16:18:30 -0300

    style page

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         url = `https://api.whatsapp.com/send?phone=${formatedPhone}&text=${formatedMessage}`
         setLink(url)
         setDisplayGenerate(false)
-      }     
+      }
     }
   }
 
@@ -63,101 +63,106 @@ function App() {
   function newLink(e) {
     setLink('')
     setDisplayGenerate(true)
-  } 
+  }
 
   return (
-    <main className="page-home">
+    <main>
+
+      <section className="generate-link">
+      <form
+        onSubmit={(e) => handleGenerateLink(e)}
+        className="generate-link-form form-container"
+        style={{ display: displayGenerate ? "block" : "none" }}
+      >
+        <div className="input-container" id="phone-input-container">
+          <label htmlFor="phone-number">Número de WhatsApp da sua Empresa</label>
+          <PhoneInput
+            name="phone-number"
+            defaultCountry="BR"
+            placeholder="00 000000000"
+            onChange={setPhone}
+            className="input"
+          />
+        </div>
+
+        <div className="input-container">
+          <label htmlFor="message">Mensagem pré definida</label>
+          <textarea
+            name="message"
+            placeholder="Escreva sua mensagem"
+            value={message}
+            className="input"
+            onChange={(e) => setMessage(e.target.value)}></textarea>
+        </div>
+
+        <button type="submit" className="btn">
+          <span className="icon-button">
+            <FiLink2 size={24} />
+          </span>
+          Gerar link de WhatsApp
+        </button>
+      </form>
+
+        
       <section className="initial">
         <div className="page-name">
-          <h1>Gerador de links para Whatsapp</h1>
+          <h1>Atenda mais clientes e aumente suas vendas!</h1>
 
-          <ol>
-            <li>Entre com seu número de telefone.</li>
-            <li>Escreva a mensagem padrão que será exibida.</li>
-            <li>Clique em gerar link.</li>
-            <li>Copie o link e use como quiser!</li>
-          </ol>
+
+          <h2>Acesse o material gratuito com Guia de Vantagens da API oficial do WhatsApp Business.</h2>
+          <img className= "redes-sociais" src="./logo-redes.png"></img>
+          <button type="submit" className="btn-material">
+            <span className="icon-button-material">
+            </span>
+            Acessar Material
+          </button>
+
         </div>
       </section>
 
-      <section className="generate-link">
-        <form
-          onSubmit={(e) => handleGenerateLink(e)}
-          className="generate-link-form form-container"
-          style={{ display: displayGenerate ? "block" : "none" }}
+      <article
+        className="copy-link-form form-container"
+        style={{ display: !displayGenerate ? "block" : "none" }}
+      >
+        <div className="input-container">
+          <label htmlFor="link-long">Link</label>
+          <div className="display-link">
+            <input id="link-long" value={link} placeholder={link} readOnly className="input-display" />
+            <button
+              onClick={(e) => copyLink(e, 'link-long')}
+              className="btn-copy"
+            >
+              <FiCopy size={24} />
+            </button>
+          </div>
+        </div>
+
+        <div className="input-container">
+          <label htmlFor="link-short">Link encurtado</label>
+          <div className="display-link">
+            <p id="link-short"></p>
+            <button
+              onClick={(e) => copyLink(e, 'link-short')}
+              className="btn-copy"
+            >
+              <FiCopy size={24} />
+            </button>
+          </div>
+        </div>
+
+        <button
+          className="btn"
+          onClick={(e) => newLink(e)}
         >
-          <div className="input-container" id="phone-input-container">
-            <label htmlFor="phone-number">Número do celular</label>
-            <PhoneInput
-              name="phone-number"
-              defaultCountry="BR"
-              placeholder="00 000000000"
-              onChange={setPhone}
-              className="input"
-            />
-          </div>
+          <span className="icon-button">
+            <FiRepeat size={24} />
+          </span>
+          Novo link
+        </button>
+      </article>
 
-          <div className="input-container">
-            <label htmlFor="message">Mensagem</label>
-            <textarea
-              name="message"
-              placeholder="Escreva sua mensagem"
-              value={message}
-              className="input"
-              onChange={(e) => setMessage(e.target.value)}></textarea>
-          </div>
-
-          <button type="submit" className="btn">
-            <span className="icon-button">
-              <FiLink2 size={24} />
-            </span>
-            Gerar link
-          </button>
-        </form>
-
-        <article
-          className="copy-link-form form-container"
-          style={{ display: !displayGenerate ? "block" : "none" }}
-        >
-          <div className="input-container">
-            <label htmlFor="link-long">Link</label>
-            <div className="display-link">
-              <input id="link-long" value={link} placeholder={link} readOnly className="input-display" />
-              <button
-                onClick={(e) => copyLink(e, 'link-long')}
-                className="btn-copy"
-              >
-                <FiCopy size={24} />
-              </button>
-            </div>
-          </div>
-
-          <div className="input-container">
-            <label htmlFor="link-short">Link encurtado</label>
-            <div className="display-link">
-              <p id="link-short"></p>
-              <button
-                onClick={(e) => copyLink(e, 'link-short')}
-                className="btn-copy"
-              >
-                <FiCopy size={24} />
-              </button>
-            </div>
-          </div>
-
-          <button
-            className="btn"
-            onClick={(e) => newLink(e)}
-          >
-            <span className="icon-button">
-              <FiRepeat size={24} />
-            </span>
-            Novo link
-          </button>
-        </article>
-
-      </section>
-    </main>
+    </section>
+    </main >
   );
 }
 
